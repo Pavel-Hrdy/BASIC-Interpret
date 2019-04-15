beaf18c1c98215a5e40be818937b83bc994756fc
PavelHrdy <pajus89@gmail.com>
2019-04-15 23:42:44 +0200

    Project files edit

--- cpp_basic_interpret/cpp_basic_interpret.cpp
+++ cpp_basic_interpret/cpp_basic_interpret.cpp
@@ -6,7 +6,19 @@
 #include <vector>
 #include "lexer.h"
 #include "exceptions.h"
+#include "icvm.h"
+#include "parser.h"
 
 int main()
 {
+	
+		std::string code = "10 PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE OF X IS \";X";
+		std::vector<TType> types;
+		Lexer l(code);
+
+		while (true) {
+			Token t = l.GetToken();
+			if (t.GetTokenType()->Type() == TType::EndOfCode) break;
+			types.push_back(t.GetTokenType()->Type());
+		}
 }
