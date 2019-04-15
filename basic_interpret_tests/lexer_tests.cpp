82dd769ed6335fc1367a296bbb25c8413859fa6f
PavelHrdy <pajus89@gmail.com>
2019-04-15 23:42:28 +0200

    Tests edit because of new API

--- basic_interpret_tests/lexer_tests.cpp
+++ basic_interpret_tests/lexer_tests.cpp
@@ -21,116 +21,132 @@ namespace basic_interpret_tests
 
 		TEST_METHOD(EmptyCodeToken)
 		{
-			std::string code = "";
-
+			std::string code = "10";
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 			TokenType * token = t.GetTokenType();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual((int)TType::EndOfCode, (int)token->Type());
 		}
 
 		TEST_METHOD(StringVariableToken)
 		{
-			std::string code = "test$";
+			std::string code = "10 test$";
 
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 			TokenType * token = t.GetTokenType();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual((int)TType::StringVariable, (int)token->Type());
 		}
 
 		TEST_METHOD(VariableToken)
 		{
-			std::string code = "test";
+			std::string code = "10 test";
 
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 			TokenType * token = t.GetTokenType();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual((int)TType::Variable, (int)token->Type());
 		}
 
 		TEST_METHOD(IntToken)
 		{
-			std::string code = "50";
+			std::string code = "10 50";
 
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 			TokenType * token = t.GetTokenType();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual((int)TType::Int, (int)token->Type());
 		}
 
 		TEST_METHOD(Real)
 		{
-			std::string code = "50.86";
+			std::string code = "10 50.86";
 
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 			TokenType * token = t.GetTokenType();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual((int)TType::Real, (int)token->Type());
 		}
 
 		TEST_METHOD(RealToken)
 		{
-			std::string code = "50.86";
+			std::string code = "10 50.86";
 
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 			TokenType * token = t.GetTokenType();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual((int)TType::Real, (int)token->Type());
 		}
 
 		TEST_METHOD(RealToken_TestParse)
 		{
-			std::string code = "50.86";
+			std::string code = "10 50.86";
 
 			Lexer l(code);
-			Token t = l.GetNextToken();
+			Token lineNumber = l.GetToken();
+			Token t = l.GetToken();
 
+			Assert::AreEqual((int)TType::Int, (int)lineNumber.GetTokenType()->Type());
 			Assert::AreEqual(50.86, std::stod(t.GetContent()));
 		}
 
 		TEST_METHOD(ComplexTest_CorrectInput)
 		{
-			std::string code = "PRINT X,Y,Z,A$\n100 PRINT\"THE VALUE OF X IS \";X";
+			std::string code = "10 PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE OF X IS \";X";
 			std::vector<TType> types;
 			Lexer l(code);
 
 			while (true) {
-				Token t = l.GetNextToken();
+				Token t = l.GetToken();
 				if (t.GetTokenType()->Type() == TType::EndOfCode) break;
 				types.push_back(t.GetTokenType()->Type());
 			}
-			Assert::IsTrue(types.size() == 13);
-			Assert::IsTrue(types[0] == TType::Print_F);
-			Assert::IsTrue(types[1] == TType::Variable);
-			Assert::IsTrue(types[2] == TType::Comma);
-			Assert::IsTrue(types[3] == TType::Variable);
-			Assert::IsTrue(types[4] == TType::Comma);
-			Assert::IsTrue(types[5] == TType::Variable);
-			Assert::IsTrue(types[6] == TType::Comma);
-			Assert::IsTrue(types[7] == TType::StringVariable);
-			Assert::IsTrue(types[8] == TType::Int);
-			Assert::IsTrue(types[9] == TType::Print_F);
-			Assert::IsTrue(types[10] == TType::String);
-			Assert::IsTrue(types[11] == TType::Semicolon);
-			Assert::IsTrue(types[12] == TType::Variable);
+			Assert::IsTrue(types.size() == 16);
+			Assert::IsTrue(types[0] == TType::Int);
+			Assert::IsTrue(types[1] == TType::Print_F);
+			Assert::IsTrue(types[2] == TType::Variable);
+			Assert::IsTrue(types[3] == TType::Comma);
+			Assert::IsTrue(types[4] == TType::Variable);
+			Assert::IsTrue(types[5] == TType::Comma);
+			Assert::IsTrue(types[6] == TType::Variable);
+			Assert::IsTrue(types[7] == TType::Comma);
+			Assert::IsTrue(types[8] == TType::StringVariable);
+			Assert::IsTrue(types[9] == TType::NewLine);
+			Assert::IsTrue(types[10] == TType::Int);
+			Assert::IsTrue(types[11] == TType::Int);
+			Assert::IsTrue(types[12] == TType::Print_F);
+			Assert::IsTrue(types[13] == TType::String);
+			Assert::IsTrue(types[14] == TType::Semicolon);
+			Assert::IsTrue(types[15] == TType::Variable);
 		}
 
 		TEST_METHOD(ComplexTest_UnknownCharacter)
 		{
-			std::string code = "PRINT X,Y,Z,A$\n100 PRINT\"THE VALUE OF X IS \";X????";
+			std::string code = "10 PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE OF X IS \";X????";
 			std::vector<TType> types;
 			Lexer l(code);
 
 			try {
 				while (true) {
-					Token t = l.GetNextToken();
+					Token t = l.GetToken();
 					if (t.GetTokenType()->Type() == TType::EndOfCode) { break; };
 					types.push_back(t.GetTokenType()->Type());
 				}
@@ -146,13 +162,13 @@ namespace basic_interpret_tests
 
 		TEST_METHOD(ComplexTest_NewlineInString)
 		{
-			std::string code = "PRINT X,Y,Z,A$\n100 PRINT\"THE VALUE \nOF X IS \";X";
+			std::string code = "10 PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE \nOF X IS \";X";
 			std::vector<TType> types;
 			Lexer l(code);
 
 			try {
 				while (true) {
-					Token t = l.GetNextToken();
+					Token t = l.GetToken();
 					if (t.GetTokenType()->Type() == TType::EndOfCode) break;
 					types.push_back(t.GetTokenType()->Type());
 				}
@@ -168,13 +184,13 @@ namespace basic_interpret_tests
 
 		TEST_METHOD(ComplexTest_StringNotTerminated)
 		{
-			std::string code = "PRINT X,Y,Z,A$\n100 PRINT\"THE VALUE OF X IS ;X";
+			std::string code = "10 PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE OF X IS ;X";
 			std::vector<TType> types;
 			Lexer l(code);
 
 			try {
 				while (true) {
-					Token t = l.GetNextToken();
+					Token t = l.GetToken();
 					if (t.GetTokenType()->Type() == TType::EndOfCode) break;
 					types.push_back(t.GetTokenType()->Type());
 				}
@@ -184,8 +200,33 @@ namespace basic_interpret_tests
 			}
 			catch (std::exception) {
 				Assert::IsFalse(true);
+			
 			}
 
+		}
+
+		TEST_METHOD(MissingLineNumberAtTheBeginning) {
+			std::string code = "PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE OF X IS ;X";
+			Lexer l(code);
+			try {
+				l.GetToken();
+			}
+			catch (LineNumberNotFoundException) {
+				Assert::IsTrue(true);
+			}
+			catch (std::exception) { Assert::IsFalse(true); }
+		}
+
+		TEST_METHOD(WrongLineNumberAtTheBeginning) {
+			std::string code = "20 PRINT X,Y,Z,A$\n20 100 PRINT\"THE VALUE OF X IS ;X";
+			Lexer l(code);
+			try {
+				l.GetToken();
+			}
+			catch (WrongLineNumberException) {
+				Assert::IsTrue(true);
+			}
+			catch (std::exception) { Assert::IsFalse(true); }
 		}
 	};
 }
